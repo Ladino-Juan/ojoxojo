52f1460d6253bf806e5b2709da0155f6c455ee2b
David Ladino <juanlc1526@gmail.com>
2023-09-29 16:01:29 -0500

    ahora si x2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,28 @@ import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const handleRoute = () => {
-    navigate("/daniel_bedoya");
+  const [nav, setNav] = useState(false);
+
+  const toggleNav = () => {
+    setNav(!nav);
   };
 
-  const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav);
+  const closeNav = () => {
+    setNav(false);
+  };
 
   return (
     <div
-      className={
-        !nav
-          ? "w-full h-[50px] bg-amarillo fixed top-0 z-50"
-          : "bg-black w-full h-[50px] fixed top-0 z-50"
-      }
+      className={`w-full h-[50px] fixed top-0 z-50 ${
+        nav ? "bg-black" : "bg-amarillo"
+      }`}
     >
       <div className="px-2 flex justify-around items-center w-full h-full text-xs">
         <a
-          onClick={() => navigate("/")}
+          onClick={() => {
+            navigate("/");
+            closeNav();
+          }}
           href="/"
           aria-label="ojoxojo tattoo studio home"
         >
@@ -33,14 +37,17 @@ const NavBar = () => {
                 : "assets/ojoxojo_logo_white.webp"
             }
             alt="ojoxojo tattoo logo"
-          ></img>
+          />
         </a>
 
         <div className="hidden md:flex text-black font-libre">
-          <ul className="hidden md:flex">
+          <ul className="md:flex">
             <li className="pr-4 relative group">
               <a
-                onClick={handleRoute}
+                onClick={() => {
+                  navigate("/daniel_bedoya");
+                  closeNav();
+                }}
                 className="hover:text-blanco cursor-pointer transition-all duration-700"
                 aria-label="tatuajes realizados en ojoxojo tattoo studio"
               >
@@ -49,8 +56,9 @@ const NavBar = () => {
             </li>
             <li className="pr-4 relative group">
               <a
-                className="hover:text-blanco transition-all duration-700"
                 href="#contacto"
+                onClick={closeNav}
+                className="hover:text-blanco transition-all duration-700"
                 aria-label="ojoxojo tattoo studio contacto"
               >
                 CONTACTO
@@ -58,9 +66,12 @@ const NavBar = () => {
             </li>
             <li className="pr-4 relative group">
               <a
-              onClick={() => navigate("/#patrocinadores")}
-                className="hover:text-blanco transition-all duration-700"
+                onClick={() => {
+                  navigate("/#patrocinadores");
+                  closeNav();
+                }}
                 href="/#patrocinadores"
+                className="hover:text-blanco transition-all duration-700"
                 aria-label="ojoxojo tattoo studio patrocinadores"
               >
                 PATROCINADORES
@@ -69,37 +80,43 @@ const NavBar = () => {
           </ul>
         </div>
 
-        <div className="md:hidden ml-16 text-black" onClick={handleClick}>
-          {!nav ? (
-            <MenuIcon className="w-5" />
-          ) : (
-            <XIcon className="w-5 text-blanco" />
-          )}
+        <div className="md:hidden ml-16 text-black" onClick={toggleNav}>
+          {nav ? <XIcon className="w-5 text-blanco" /> : <MenuIcon className="w-5" />}
         </div>
       </div>
       <ul
-        className={
-          !nav
-            ? "hidden"
-            : "absolute bg-black text-blanco text-center w-full h-screen px-8 py-12 rounded-b-xl font-libre"
-        }
+        className={`${
+          nav
+            ? "absolute bg-black text-blanco text-center w-full h-screen px-8 py-12 rounded-b-xl font-libre"
+            : "hidden"
+        }`}
       >
         <li className="pr-4 relative group">
           <a
-            onClick={handleRoute}
+            onClick={() => {
+              navigate("/daniel_bedoya");
+              closeNav();
+            }}
             aria-label="tatuajes realizados en ojoxojo tattoo studio"
           >
             GALERIA
           </a>
         </li>
         <li className="pr-4 relative group">
-          <a href="#contacto" aria-label="ojoxojo tattoo studio contacto">
+          <a
+            onClick={closeNav}
+            href="#contacto"
+            aria-label="ojoxojo tattoo studio contacto"
+          >
             CONTACTO
           </a>
         </li>
         <li className="pr-4 relative group">
           <a
-            onClick={() => navigate("/#patrocinadores")}
+            onClick={() => {
+              navigate("/#patrocinadores");
+              closeNav();
+            }}
             href="/#patrocinadores"
             aria-label="ojoxojo tattoo studio patrocinadores"
           >
